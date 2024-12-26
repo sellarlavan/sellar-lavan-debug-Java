8bcfcd593a6c8021361c6aa8873592b43e9c8939
Lavan Sellar <lavansellar@gmail.com>
2024-12-26 09:37:56 +0100

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,30 +7,22 @@ import java.util.*;
 
 
 public class AnalyticsCounter {
-	
-	public static void main(String args[]) throws Exception {
 
-//		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
-//		String line = reader.readLine();
-//
+	ISymptomReader reader;
+	ISymptomWriter writer;
 
-//
-//		while (line != null){
-//			if(symptomsMap.containsKey(line)){
-//				symptomsMap.put(line, symptomsMap.get(line) + 1);
-//			}
-//			else{
-//				symptomsMap.put(line, 1);
-//			}
-//
-//			line = reader.readLine();
-//		}
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer){
+		this.reader = reader;
+		this.writer = writer;
+	}
 
-		ReadSymptomDataFromFile symptomDataFromFile = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+	public List<String> getSymptoms(){
+		return reader.GetSymptoms();
+	}
 
-		List<String> symptoms = symptomDataFromFile.GetSymptoms();
-
+	public Map<String, Integer> countSymptoms(List<String> symptoms){
 		Map<String, Integer> symptomsMap = new HashMap<String, Integer>();
+
 		for(int i = 0; i < symptoms.size(); i++ ){
 			String line = symptoms.get(i);
 			if(symptomsMap.containsKey(line)){
@@ -38,12 +30,53 @@ public class AnalyticsCounter {
 			}
 			else{
 				symptomsMap.put(line, 1);
-
 			}
 		}
 
+		return symptomsMap;
+	}
 
-		WriteSymptomsDataToFile dataToFile = new WriteSymptomsDataToFile();
-		dataToFile.writeSymptoms(symptomsMap);
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+
+		List<Map.Entry<String, Integer>> sortedList = new ArrayList<>(symptoms.entrySet());
+
+
+		sortedList.sort(Map.Entry.comparingByKey());
+
+
+		Map<String, Integer> sortedSymptomsMap = new LinkedHashMap<>();
+		for (Map.Entry<String, Integer> entry : sortedList) {
+			sortedSymptomsMap.put(entry.getKey(), entry.getValue());
+		}
+
+		return sortedSymptomsMap;
+	}
+
+	public void writeSymptoms(Map<String, Integer> symptoms){
+		writer.writeSymptoms(symptoms);
+	}
+
+	public static void main(String args[]) throws Exception {
+
+	ISymptomReader reader1 = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+	ISymptomWriter writer1 = new WriteSymptomsDataToFile();
+	AnalyticsCounter counter = new AnalyticsCounter(reader1, writer1);
+
+	List<String> list = counter.getSymptoms();
+	Map<String,Integer> listCount = counter.countSymptoms(list);
+	listCount = counter.sortSymptoms(listCount);
+	counter.writeSymptoms(listCount);
+
+	Map<String, Integer> test = new HashMap<>();
+	test.put("rose", 2);
+	test.put("argent", 3);
+	test.put("bleu", 1);
+	test.put("vert", 8);
+
+	List<Map.Entry<String,Integer>> sortList = new ArrayList<>(test.entrySet());
+	sortList.sort(Map.Entry.comparingByValue());
+	System.out.println(sortList);
+
+
 	}
 }
